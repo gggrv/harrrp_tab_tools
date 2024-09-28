e8b5707bad1bb0feaf33bb956b17d8777bd5da5a
gggrv <43758914+gggrv@users.noreply.github.com>
2024-09-28 22:42:52 +0300

    [qol] fast launcher for midi Type1 compliance

---
+++ autorun_MidiType1Force.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+#---------------------------------------------------------------------------+++
+
+# logging
+import logging
+log = logging.getLogger(__name__)
+
+# embedded in python
+import os
+# pip install
+# same project
+from src.main_environment import AVAILABLE_TAB_FOLDERS
+from src.some_MidiText_Columns import some_Columns_MidiText
+
+#---------------------------------------------------------------------------+++
+    
+def autorun():
+    
+    # make sure folders are ok
+    if AVAILABLE_TAB_FOLDERS is None:
+        return
+    
+    for tabs_folder in AVAILABLE_TAB_FOLDERS:
+        
+        print( f'\n\n--- inspecting folder --- {tabs_folder}' )
+        
+        # iterate contents of the tabs folder
+        for root, subs, fs in os.walk( tabs_folder ):
+            
+            # iterate each file
+            for f in fs:
+                
+                src = os.path.join( root, f )
+                
+                # make sure it is midi
+                if not f.endswith( '.mid' ):
+                    continue
+                
+                some_Columns_MidiText.ensure_compliance_with_type1_midi_format( src )
+            
+    print( 'DONE' )
+
+if __name__ == '__main__':
+    autorun()
+
+#---------------------------------------------------------------------------+++
+# 2024.09.28
